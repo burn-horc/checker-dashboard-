c1c069b6ce8c4c4b758149dcfe52372ffa9d1976
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-06 15:58:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ app.post("/check", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
