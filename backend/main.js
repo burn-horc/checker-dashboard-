ed37e46bdc29d47dac5d7d4f3afec0cf5454496a
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-06 12:07:55 +0800

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,28 @@
+import fetch from "node-fetch";
 
+export async function checkCookie(cookie) {
+  try {
+    const res = await fetch("https://www.netflix.com/browse", {
+      headers: {
+        cookie: cookie,
+        "user-agent":
+          "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X)"
+      }
+    });
+
+    if (res.status === 200) {
+      return {
+        status: "HIT"
+      };
+    }
+
+    return {
+      status: "BAD"
+    };
+
+  } catch (err) {
+    return {
+      status: "ERROR"
+    };
+  }
+}
