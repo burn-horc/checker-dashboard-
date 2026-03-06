0b3413d8a3433c9c04ebcc1897727cfd54c34293
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-06 17:04:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const __dirname = new URL('.', import.meta.url).pathname;
-
-// serve frontend build
-app.use(express.static("dist"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
-});
 
-
+// API ROUTE
 app.get("/api/check", (req, res) => {
   res.json({
     status: "API WORKING"
   });
+});
+
+
+// SERVE FRONTEND
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
