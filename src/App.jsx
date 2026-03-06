ea4f4a5a28bd29738ea2a6b204fa1bd04b97869d
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-06 11:48:44 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import { useState } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
+export default function App() {
+  const [access, setAccess] = useState(false);
+
+  return access ? (
+    <Dashboard />
+  ) : (
+    <Login onLogin={() => setAccess(true)} />
+  );
+}
